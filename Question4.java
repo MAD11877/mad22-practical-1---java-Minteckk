5907c19984fd817f562c7fb3410784b99570cc61
Minteckk <s10228079@connect.np.edu.sg>
2022-04-19 21:09:45 +0800

    update question4

--- Question4.java
+++ Question4.java
@@ -7,7 +7,7 @@ public class Question4
     Scanner in = new Scanner(System.in);
     int b = in.nextInt();
 
-    for(int i = b; i < 5; i++)
+    for(int i = b; i > 0; i--)
     {
       for(int a = 0; a < i; a++)
       {
