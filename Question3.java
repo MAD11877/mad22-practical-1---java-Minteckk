94a558045039f844a6185f7153c44401e0b7961f
Minteckk <s10228079@connect.np.edu.sg>
2022-04-19 20:06:03 +0800

    Update Question3

--- Question3.java
+++ Question3.java
@@ -5,7 +5,6 @@ public class Question3
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
-    System.out.println("Enter a integer");
     int integer = in.nextInt();
     int result = integer * integer;
     System.out.println(result);
